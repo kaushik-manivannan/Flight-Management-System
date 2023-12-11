8350a6943b76b7772ad30437309706ce43b8db43
Kaushik Manivannan <manivannan.k@northeastern.edu>
2023-12-10 23:23:14 -0500

    Add Title for AddLocalFlight

--- src/main/java/edu/neu/csye6200/AddLocalFlight.java
+++ src/main/java/edu/neu/csye6200/AddLocalFlight.java
@@ -37,6 +37,7 @@ public class AddLocalFlight extends JFrame {
 	public AddLocalFlight() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 537, 359);
+		setTitle("Add Local Flight");
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
