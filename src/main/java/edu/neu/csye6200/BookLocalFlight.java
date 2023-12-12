49a18998e6c6c8c90256233f2678ed233349c95b
ChakradharAbhinay <145234036+ChakradharAbhinay@users.noreply.github.com>
2023-12-11 19:07:25 -0500

    modified BookLocalFlight.java-2

--- src/main/java/edu/neu/csye6200/BookLocalFlight.java
+++ src/main/java/edu/neu/csye6200/BookLocalFlight.java
@@ -43,3 +43,60 @@ public class  BookLocalFlight extends JFrame {
 /**
  * Create the frame.
  */
+public BookLocalFlight() {
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    setBounds(100, 100, 843, 419);
+    setTitle("Book Local DomesticFlight");
+    contentPane = new JPanel();
+    contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+    setContentPane(contentPane);
+    contentPane.setLayout(null);
+
+    JLabel lblNewLabel = new JLabel("Local DomesticFlight Schedule");
+    lblNewLabel.setFont(new Font("Calibri", Font.BOLD, 15));
+    lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+    lblNewLabel.setBounds(28, 0, 679, 28);
+    contentPane.add(lblNewLabel);
+
+    String[] columns = {"DomesticFlight ID", "Time", "Date", "Daparture",
+            "Arrival" , "Economy class Seats" , "Business class Seats" , "Time Duration"};
+
+    DefaultTableModel obj=new DefaultTableModel(columns,0);
+
+    for (int i = 0; i< LocalFlightSchedule.domesticFlightList.size() ; i++)
+    {
+
+        Object[] update= {LocalFlightSchedule.domesticFlightList.get(i).getFlightID() , LocalFlightSchedule.domesticFlightList.get(i).getDepartTime() , LocalFlightSchedule.domesticFlightList.get(i).getLandTime()
+                , LocalFlightSchedule.domesticFlightList.get(i).getDepartDestination() , LocalFlightSchedule.domesticFlightList.get(i).getLandDestination() ,
+                LocalFlightSchedule.domesticFlightList.get(i).getEconomySeats() , LocalFlightSchedule.domesticFlightList.get(i).getBusinessSeats() , LocalFlightSchedule.domesticFlightList.get(i).getDistance()};
+
+        obj.addRow(update);
+    }
+
+    flightTable = new JTable(obj);
+    JScrollPane scrollPane = new JScrollPane(flightTable);
+    scrollPane.setBounds(10, 39, 764, 200);
+    contentPane.add(scrollPane);
+
+    JButton btnNewButton = new JButton("Cancel DomesticFlight");
+    btnNewButton.addActionListener(e -> {
+
+        for (Passengers x: SignUP.PassengerList)
+        {
+            if (x.getUsername().equals(SignUP.username.getText()))
+            {
+                if (x.getBooked()==true)
+                {
+                    double hour=Integer.valueOf((String) flightTable.getModel().getValueAt(flightTable.getSelectedRow(), 7));
+
+                    double price=(hour*10000)+((hour*10000)*5/100);
+                    JOptionPane.showMessageDialog(null, "Your flight has been cancelled successfully with a penalty of "+ (price*25/100));
+                    SignUP.PassengerList.get(0).setBooked(false);
+                }
+                else
+                {
+                    JOptionPane.showMessageDialog(null, "Please book your flight first.");
+                }
+            }
+        }
+    });
